cc287e75522f574be691efad024145c394f7bc60
Moreira Luna <mmoreiral@minsait.com>
2022-11-21 09:18:39 -0300

    add a new menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RedesSociaisComponent } from './components/redes-sociais/redes-sociais.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RedesSociaisComponent
   ],
   imports: [
     BrowserModule,
